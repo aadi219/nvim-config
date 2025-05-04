5f06fa750281962d77a6c407fb796e357964b155
aadi219 <aadi@Badolas>
2025-05-04 00:47:21 -0400

    update lsp-config virtual text

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,15 +17,31 @@ return {
     },
     {
         'neovim/nvim-lspconfig',
-        config = function()
+        opts = function()
+            local ret = {
+                diagnostics = {
+                    underline = true,
+                    virtual_text = {
+                        spacing = 4,
+                        prefix = "●",
+                    },
+                },
+                inlay_hints = {
+                    enabled = true,
+                }
+            }
+            return ret
+        end,
+        config = function(_, opts)
             local lspconfig = require('lspconfig')
+            vim.diagnostic.config(opts.diagnostics)
             lspconfig.lua_ls.setup({})
             lspconfig.eslint.setup({})
             lspconfig.tsserver.setup({})
+            lspconfig.clangd.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action,{})
         end
     }
-
 }
