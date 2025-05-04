e377437c95506390357a816bd63182ae4bb19d3a
aadi219 <aadi@Badolas>
2025-05-04 00:49:23 -0400

    new keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,4 +1,3 @@
-vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
@@ -8,3 +7,5 @@ vim.cmd('set relativenumber')
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-n>", ":tabnew <CR>")
+vim.keymap.set("n", "<leader>fl", ":norm va{ zf <CR>") -- fold current function block
+vim.keymap.set("n", "<leader>fo", "zd") -- open folded function block
